de506ee2d1fa394af497eabb62c8c0ebe5ba4401
xinxin.yuan <xinxin.yuan@trip.com>
2022-10-14 09:05:48 +0800

    完善仓库文档

---
+++ README.md
@@ -0,0 +1,2 @@
+#### 仓库文档
+1. @babel/plugin-transform-runtime 可以减小文件编译后体积，检查代码改为require方式，而不是简单的注入到文件中去；
--- dist/index.js
+++ dist/index.js
@@ -1,25 +1,18 @@
 "use strict";
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
-
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+var _classCallCheck2 = _interopRequireDefault(require("@babel/runtime/helpers/classCallCheck"));
+var _createClass2 = _interopRequireDefault(require("@babel/runtime/helpers/createClass"));
 var Person1 = /*#__PURE__*/function () {
   function Person1(name) {
-    _classCallCheck(this, Person1);
-
+    (0, _classCallCheck2["default"])(this, Person1);
     this.name = name;
   }
-
-  _createClass(Person1, [{
+  (0, _createClass2["default"])(Person1, [{
     key: "say",
     value: function say() {}
   }]);
-
   return Person1;
 }();
-
 var person = new Person("test");
 person.say();
